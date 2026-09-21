5102086d8b52a9b5c14aeb26ca4eb925d5f4629b
chelcfelize <egaranchelseafelize@gmail.com>
2026-09-21 17:55:26 +0800

    nako po

--- journal/src/App.jsx
+++ journal/src/App.jsx
@@ -1,8 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
 
+function Intro() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('3 seconds passed!')
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <div className="intro">
+      <h1>proof: we were here.</h1>
+    </div>
+  )
+}
+
+
+function Home(){
+
+    return(
+      <h1>well hello hello WAW MALI MALI AH</h1>
+    )
+}
+
 function App() {
-  
+  return(
+    /*<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>*/
+    <Intro />
+  )
 }
 
 export default App
