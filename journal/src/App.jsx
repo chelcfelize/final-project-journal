b6c59b2d56b7c1cb92278fce66cb09b9e038d647
chelcfelize <egaranchelseafelize@gmail.com>
2026-09-21 21:58:29 +0800

    to be done:calendar

--- journal/src/App.jsx
+++ journal/src/App.jsx
@@ -4,6 +4,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 
+
+
 function Intro(){
   return (
     <div className='intro'>
@@ -11,20 +13,122 @@ function Intro(){
     </div>
   )
 }
-
-function Home(){
+function Navbar(){
+  const [showAddMenu, setShowAddMenu] = useState(false)
  return(
-   <nav className='homelinks'>
+  <>
+   <div className='navbar'>
+    <nav className='homelinks'>
     <a href="/home">Home</a>
-      <a href="/add">Add</a>
+
+      <div className="add-menu">
+        <button onClick={() => setShowAddMenu(!showAddMenu)}>
+          Add
+        </button>
+
+        {showAddMenu && (
+          <div className="popup-menu">
+            <a href="/addEntry">AddEntry</a>
+             <a href="/addPhoto">AddPhoto</a>
+          </div>
+        )}
+      </div>
+
       <a href="/calendar">Calendar</a>
+      <h2 className='to-the-right'>proof: we were here.</h2>
   </nav>
+   </div>
+   </>
+ )
+}
+function Home(){
+ return (
+   <>
+   <Navbar />
+   </>
  )
 }
 
+function AddEntry(){
+  return (
+   <>
+   <Navbar />
+
+   <div className="entry-form">
+      <form>
+        <label>
+          <input type="text" name="entry-title" placeholder='add entry title'/>
+        </label>
+
+        <label>
+          <textarea name="journal-content" placeholder='write about a moment...'></textarea>
+        </label>
+
+        <button type="submit">publish proof</button>
+      </form>
+    </div>
+   </>
+ )
+}
+
+function AddPhoto(){
+  const [image, setImage] = useState(null)
+
+  function handleImageChange(event) {
+    const file = event.target.files[0]
+
+    if (file) {
+      setImage(URL.createObjectURL(file))
+    }
+  }
+  return (
+   <>
+   <Navbar />
+
+   <div className="photo-form">
+      <form>
+        <label>
+          <input type="text" name="photo-title" placeholder='add moment title'/>
+        </label>
+
+        <label>
+          <input type="text" name="photo-caption" placeholder='add photo caption...'/>
+        </label>
+
+        <label className="select-image">
+        Select Image
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+      </label>
+
+      {image && (
+        <img
+          src={image}
+          alt="Selected"
+          className="image-preview"
+        />
+      )}
+
+        <button type="submit">publish proof</button>
+      </form>
+    </div>
+   </>
+ )
+}
+
+function Calendar(){
+ return(
+  <>
+    <Navbar />
+   
+   
+  </>
+ )
+}
+
+
 function App() {
   return(
-    <Home />
+    <Calendar />
   )
 }
 
